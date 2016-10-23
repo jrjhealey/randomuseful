fe60a5c9bbea016c2ddf8bd8aefc7e43ab0d08b7
Joe Healey <jrj.healey@gmail.com>
2016-10-23 19:54:52 +0100

    Add files via upload

---
+++ texshop_colors.sh
@@ -0,0 +1,31 @@
+defaults write TeXShop insertionpoint_R 0.00
+defaults write TeXShop insertionpoint_G 0.00
+defaults write TeXShop insertionpoint_B 0.00
+
+defaults write TeXShop markerred 0.02
+defaults write TeXShop markergreen 0.51
+defaults write TeXShop markerblue 0.13
+
+defaults write TeXShop background_R 0.99;
+defaults write TeXShop background_G 0.96;
+defaults write TeXShop background_B 0.89;
+
+# commands = solarized red = 220  50  47
+defaults write TeXShop commandred 0.86;
+defaults write TeXShop commandgreen 0.196;
+defaults write TeXShop commandblue 0.184;
+
+# comments = solarized base1 = 147 161 161
+defaults write TeXShop commentred 0.58;
+defaults write TeXShop commentgreen 0.63;
+defaults write TeXShop commentblue 0.63;
+
+# foreground = solarized base00 = 101 123 131
+defaults write TeXShop foreground_R 0.40;
+defaults write TeXShop foreground_G 0.48;
+defaults write TeXShop foreground_B 0.51;
+
+# index = solarized magenta = 211 54 130
+defaults write TeXShop indexred 0.83;
+defaults write TeXShop indexgreen 0.21;
+defaults write TeXShop indexblue 0.51;

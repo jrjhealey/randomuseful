8115e98b3f998cd950a56f9e521f0b76eed432f2
jrjhealey <jrj.healey@gmail.com>
2017-04-03 12:41:06 +0100

    added solarized2

--- texshop_colors.sh
+++ texshop_colors.sh
@@ -4,6 +4,37 @@
 #Themes:
 # Light background, red text
 
+solarized2(){
+defaults write TeXShop background_R 0.1315524628925428
+defaults write TeXShop background_G 0.1514611456578815
+defaults write TeXShop background_B 0.1584508659361748
+
+defaults write TeXShop foreground_R 0.7675526591846704
+defaults write TeXShop foreground_G 0.7802913168283133
+defaults write TeXShop foreground_B 0.7848272996704031
+
+defaults write TeXShop commentred 0.456989727381416
+defaults write TeXShop commentgreen 0.4686302345468037
+defaults write TeXShop commentblue 0.47277872487402767
+
+defaults write TeXShop commandred 0.5152778241117596
+defaults write TeXShop commandgreen 0.7287980512108025
+defaults write TeXShop commandblue 0.3277610086866684
+
+defaults write TeXShop indexred 1.0
+defaults write TeXShop indexgreen 0.44605507023517926
+defaults write TeXShop indexblue 0.6444164367288312
+
+defaults write TeXShop markerred 0.0
+defaults write TeXShop markergreen 0.6473821768286941
+defaults write TeXShop markerblue 0.5877138942034104
+
+defaults write TeXShop insertionpoint_R 0.9329004893618941
+defaults write TeXShop insertionpoint_G 0.9460941572562335
+defaults write TeXShop insertionpoint_B 0.950788625916625
+}
+
+
 pale(){
   echo "Setting pale theme."
 defaults write TeXShop insertionpoint_R 0.00
@@ -209,8 +240,10 @@ while getopts "ht:" OPTION ;do
 done
 
 if [[ -z $theme ]]; then
-      usage
-      echo "No theme sequence provided. Exiting." ; exit 1
+
+      echo "No theme identifier provided. Exiting." ; exit 1
+      echo "======================================"
+	usage
 fi
 
 
